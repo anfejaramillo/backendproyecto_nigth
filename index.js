7eb489fbbb56f6655a51291b3fe4bbd908f1bc84
anfejaramillo <anfejaramillo@utp.edu.co>
2022-11-22 19:16:34 -0500

    22-11-2022

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require("express");
 var bodyParser = require("body-parser");
 
 //Cargar configuracion app WEB
-const PORT = process.env.PORT || 3500;
+const appConfig = require("./config");
 
 console.log("Inicializar la Aplicacion WEB...");
 //Inicializar una APLICACION WEB
+require("./db/dbInitializer");
 const app = express();
 
 // 1) Metodo HTTP (verbos HTTP)
@@ -15,11 +16,13 @@ const app = express();
 // 3) EL ALGORITMO QUE YO PROGRAMO PARA RESPONDER ESA PETICION
 
 console.log("Configurando Routers...");
-const userRouter = require("./routes/routerUser");
+const userDummyRouter = require("./routes/routerDummyUser");
+const userRouter = require("./routes/routerUsers");
 
 //Configurar Routers en la APP
 app.use(bodyParser.json());
 
+app.use("/api/usuariosDummy", userDummyRouter);
 app.use("/api/usuarios", userRouter);
 
 app.get(
@@ -33,10 +36,12 @@ app.get(
 console.log("Iniciando Servidor");
 
 let server = app.listen(
-    PORT,
+    appConfig.PORT,
 
     function () {
-        console.log(`La aplicacion WEB esta escuchando en el PUERTO: ` + PORT);
+        console.log(
+            `La aplicacion WEB esta escuchando en el PUERTO: ` + appConfig.PORT
+        );
     }
 );
 
