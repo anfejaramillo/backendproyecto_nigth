1622f3486792b087ecf273bdc31f4c09819c81eb
anfejaramillo <anfejaramillo@utp.edu.co>
2022-11-23 19:23:39 -0500

    23-11-2022

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
-let PORT = process.env.PORT || 8080;
-let connectionString = "mongodb://localhost:27017/";
-let dbName = "dbtest_night";
-let fullUrl = connectionString + dbName;
+// let PORT = process.env.PORT || 8080;
+// let connectionString = "mongodb://localhost:27017/";
+// let dbName = "dbtest_night";
+// let fullUrl = connectionString + dbName;
 
+let PORT = process.env.PORT || 8080;
+let connectionString =
+    "mongodb+srv://admin:admin123@cluster0.sj3ngcx.mongodb.net/?retryWrites=true&w=majority";
+let dbName = "dbtest_night";
+let fullUrl =
+    "mongodb+srv://admin:admin123@cluster0.sj3ngcx.mongodb.net/" +
+    dbName +
+    "?retryWrites=true&w=majority";
 module.exports = {
     PORT,
     connectionString,
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Cargando configuracion...");
 const express = require("express");
 var bodyParser = require("body-parser");
 let cors = require("cors");
-
+let session = require("express-session");
 //Cargar configuracion app WEB
 const appConfig = require("./config");
 
@@ -15,9 +15,28 @@ const app = express();
 //middleware
 app.use(bodyParser.json());
 app.use(cors());
+//middleware para las variables de sesion
+app.use(
+    session({
+        secret: "mipalabrasecreta",
+        cookie: { maxAge: 60000, secure: false },
+    })
+);
 // 1) Metodo HTTP (verbos HTTP)
 // 2) RUTA (VIrtual)
 // 3) EL ALGORITMO QUE YO PROGRAMO PARA RESPONDER ESA PETICION
+
+//middleware para loguear cada peticion
+app.use(function (req, res, next) {
+    if (req.session.MI_VAR > -1) {
+        req.session.MI_VAR = req.session.MI_VAR + 1;
+    } else {
+        req.session.MI_VAR = 0;
+    }
+
+    console.log(req.session);
+    next();
+});
 
 console.log("Configurando Routers...");
 const userDummyRouter = require("./routes/routerDummyUser");
@@ -28,13 +47,9 @@ const userRouter = require("./routes/routerUsers");
 app.use("/api/usuariosDummy", userDummyRouter);
 app.use("/api/usuarios", userRouter);
 
-app.get(
-    "/",
-
-    function (req, res) {
-        res.send("Home Page!");
-    }
-);
+app.get("/", function (req, res) {
+    res.send("Home Page!");
+});
 
 app.get("/imagen", async function (req, res) {
     res.sendFile(
